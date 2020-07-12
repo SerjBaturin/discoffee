7099a60529e358d696ca94165e1debab8d089620
serj <sergejbaturin1985@gmail.com>
2020-07-12 23:23:22 +0500

    Add GET & POST methods for users on server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
-const express = require("express")
-const app = express()
-const PORT = 5555
+const express = require("express");
+const app = express();
+const PORT = 5555;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const User = require("./models/user");
 
-app.use(express.static('../client'))
+dotenv.config();
+
+mongoose.connect(process.env.CONNECT, {
+  dbName: "discoffee",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// app.use(express.static("../client"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+app.get("api/users", (req, res) => {
+  User.find().then((d) => res.send(d));
+});
+
+app.post("/api/users/add", (req, res) => {
+  const user = new User(req.body);
+  user.save();
+});
 
 app.listen(PORT, () => {
-  console.log("OK ===> ", PORT, process.pid)
-})
+  console.log("OK ===> ", PORT, process.pid);
+});
